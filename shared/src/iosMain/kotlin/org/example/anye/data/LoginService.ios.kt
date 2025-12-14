0ec85084cc7dafe711b867644fcceff365067e84
Schirokow <165018073+Schirokow@users.noreply.github.com>
2025-12-14 14:58:47 +0100

    feature/location -> LoginService.ios.kt updated.

--- shared/src/iosMain/kotlin/org/example/anye/data/LoginService.ios.kt
+++ shared/src/iosMain/kotlin/org/example/anye/data/LoginService.ios.kt
@@ -3,9 +3,13 @@ package org.example.anye.data
 class LoginServiceiOS : LoginService {
 //    override val currentUser: AuthUser? = null
 
-    override fun signUp(email: String, password: String, onResult: (Boolean) -> Unit) {
-        // TODO: Implement with Firebase iOS SDK (via Cocoapods/SPM)
-        onResult(false)
+    override fun signUp(
+        email: String,
+        password: String,
+        username: String,
+        onResult: (Boolean) -> Unit
+    ) {
+        TODO("Not yet implemented")
     }
 
     override fun signIn(email: String, password: String, onResult: (Boolean) -> Unit) {
